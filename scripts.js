332f592a917af0e45dd73825d4498408b8550ef6
Andrew Gabaraev <androgabar@gmail.com>
2022-11-20 12:37:19 +0300

    new blocks and slider changes

--- scripts.js
+++ scripts.js
@@ -13,7 +13,46 @@ const swiper = new Swiper(".swiper", {
   },
   on: {
     slideChange: function (swiper) {
-      document.querySelector(".slider-start").innerHTML =
+      document.querySelector(".cards-start").innerHTML =
+        (swiper.realIndex < 10 ? "0" : "") + (+swiper.realIndex + 1);
+    },
+  },
+
+  breakpoints: {
+    // when window width is >= 320px
+    250: {
+      slidesPerView: 1,
+      spaceBetween: 32,
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 2,
+      spaceBetween: 32,
+    },
+    // when window width is >= 640px
+    630: {
+      slidesPerView: 4,
+      spaceBetween: 32,
+    },
+  },
+});
+
+const repairSwiper = new Swiper(".repair-swiper", {
+  // Optional parameters
+  direction: "horizontal",
+  slidesPerView: "4",
+  keyboardControl: true,
+  grabCursor: true,
+  loop: true,
+  spaceBetween: 32,
+
+  navigation: {
+    nextEl: ".repair-next",
+    prevEl: ".repair-prev ",
+  },
+  on: {
+    slideChange: function (swiper) {
+      document.querySelector(".repair-start").innerHTML =
         (swiper.realIndex < 10 ? "0" : "") + (+swiper.realIndex + 1);
     },
   },
